643dbd44f54b4207dabc43399d03c2ba28620e4e
BeardedTek <kenny@beardedtek.com>
2022-02-24 12:24:38 -0900

    0.3.3 (#13)
    * move www -> app
    
    - move /www to /app
    - update docker-compose.yml to reflect changes
    
    * fix navigation errors on event detail page
    
    - /app/html/js/event.js
      - simplify functions
      - buttonclick() error checking
        - check to see that crucial variables are set
    - /app/html/stub/eventdetail.html
      - update function calls for buttonClick()
    
    * event details navigation
    
    - /app/html/js/event.js
      - if referrer is set, then refresh to document.referrer.
      - rework how refresh and delete work
    - /app/html/stub/eventdetail.html
      - change change refreshing/deleting to refresh-progress/delete-progress to match event.js
      - change livestream parameters
    - /app/html/cgi-bin/event.py
      - delEvent gets executed prior to everything else
      - change sleep time before refreshing to 0.25 instead of 2
      - add more logging to debug.log

---
+++ app/html/js/event.js
@@ -0,0 +1,26 @@
+function replaceInnerHTML(id,text) { document.getElementById(id).innerHTML = text; }
+function modalAction(action,id){
+    target = document.getElementById(id)
+    switch(action){
+        case "open": target.showModal(); break;
+        case "close": target.close();
+    }
+}
+function buttonClick(id,url,target="none",progress="no",referrer=false) {
+    var a = document.createElement('a');
+    if (target && target != "none"){ a.target=target; }
+    if (referrer){ url = url.concat('&referrer=').concat(document.referrer); }
+    a.href=url;
+    if (id && progress && progress == "yes"){
+        modalAction('close',id);
+        if (typeof id == 'string' || id instanceof String){
+            id = id.concat('-progress');
+            modalAction('open',id);
+            modalContent = id.concat('-content')
+            newModalContent = document.getElementById(modalContent)
+            newModalContent.innerHTML.concat("<br/>\n This page will refresh to: ",referrer)
+        }
+        setTimeout(() => {a.click();},2000);
+    }
+    else{ a.click(); }
+  }
