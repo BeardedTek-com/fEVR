14698cd97ff3c13d88b463332f3f2ecc293651eb
BeardedTek <kenny@beardedtek.com>
2022-03-09 00:43:35 -0900

    update index.py
    add actions in index.py to allow for an event landing page

--- app/html/cgi-bin/index.py
+++ app/html/cgi-bin/index.py
@@ -15,33 +15,84 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-print('content-type: text/html\n\n')
-from config import Config
-fevr = Config()
-if fevr:
-    from frigateConfig import frigateConfig
-    frigate = fevr.config['frigate']
-    fConfig = frigateConfig(frigate['url'])
-    with open('/var/www/html/stub/index.html') as indexStub:
-        index = indexStub.read()
-    with open('/var/www/html/stub/menuCamera.html') as menuCameraStub:
-        menuCameraItem = menuCameraStub.read()
-    with open('/var/www/html/stub/menuObject.html') as menuObjectStub:
-        menuObjectItem = menuObjectStub.read()
-        menu = ""
-    if not fConfig.error:
-        for camera in fConfig.cameras:
-            menu += f"{menuCameraItem}\n"
-            for object in fConfig.cameras[camera]['objects']:
-                menu+= f"{menuObjectItem.replace('#OBJECT#',object)}"
-            menu = menu.replace("#CAMERA#",camera)
-        index = index.replace('##MENU##',menu)
-        index = index.replace("##ERROR##","")
-    else:
-        with open('/var/www/html/stub/menuError.html') as menuError:
-            error = menuError.read()
-        index = index.replace('##MENU##',"")
-        index = index.replace("##ERROR##",error)
-    index = index.replace('#FRIGATE#',frigate['url'])
-    print(index)
+class fevr:
+    def __init__(self):
+        import cgi
+        self.input = cgi.FieldStorage()
+        if self.input.getvalue('action'):
+            self.getAction = self.input.getvalue('action')
+            self.action()
+        self.stub = "/var/www/html/stub"
+        self.execute()
 
+    def getStub(self,stub):
+        with open(stub) as Stub:
+            return Stub.read()
+
+    def header(self):
+        # Check to see if we should output something other than html headers.
+        variant = {"rest":"application/json","text":"text/plain"}
+        for loki in variant:
+            if self.getAction:
+                if loki == self.getAction:
+                    ctype = variant[loki]
+        if not ctype:
+            ctype = "text/html"
+        return f"content-type: {ctype}\n\n"
+
+    def action(self):
+        actions = {"event":['id','count'],"config":False}
+        url = f"{self.getAction}.py?"
+        for action in actions:
+            if action == self.getAction:
+                if actions[action]:
+                    for value in actions[action]:
+                        if self.input.getvalue(value):
+                            url += f"{value}={self.input.getvalue(value)}&"
+        self.script = f"<script>document.getElementById('contentFrame').src = '{url}';</script>\n"
+        if self.getAction == "config":
+            self.script += "<script>document.querySelector('#frigateErr').close()</script>\n"
+
+    def mainPage(self):
+        from config import Config
+        from frigateConfig import frigateConfig
+        myConfig = Config()
+        if myConfig:
+            frigateURL = myConfig.config['frigate']['url']
+            fConfig = frigateConfig(myConfig.config['frigate'])
+            index = self.getStub(f"{self.stub}/index.html")
+            menuCamera = self.getStub(f"{self.stub}/menuCamera.html")
+            menuObject = self.getStub(f"{self.stub}/menuObject.html")
+            menu=""
+            if not fConfig.error:
+                for camera in fConfig.cameras:
+                    menu+=f"{menuCamera}\n"
+                    for object in fConfig.cameras[camera]['objects']:
+                        menu+=f"{menuObject.replace('#OBJECT#',object)}"
+                    menu = menu.replace("#CAMERA#",camera)
+                index = index.replace('##MENU##',menu)
+                index = index.replace("##ERROR##","")
+            else:
+                menuError = self.getStub(f"{self.stub}/menuError.html")
+                index = index.replace('##MENU##',"")
+                index = index.replace("##ERROR##",menuError)
+            index = index.replace('#FRIGATE#',frigateURL)
+            return index
+
+    def execute(self):
+        # Print the headers    
+        index = self.header()
+        #Print the page out
+        index += self.mainPage()
+        # If there's an action, do it.
+        if self.script:
+            action = self.script
+        else:
+            action = ""
+        index = index.replace("##ACTION##",action)
+        print(index)
+
+def main():
+    fevr()
+
+main()
