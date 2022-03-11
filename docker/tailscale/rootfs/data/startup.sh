f6142761e04ac31af7c727d2338bf3d84f56b921
BeardedTek <kenny@beardedtek.com>
2022-03-11 02:16:41 -0900

    moving pieces around

--- docker/tailscale/rootfs/data/startup.sh
+++ docker/tailscale/rootfs/data/startup.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 # setup ip forwarding
-echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
-echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf
-sudo sysctl -p /etc/sysctl.conf
+echo 'net.ipv4.ip_forward = 1' | tee -a /etc/sysctl.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.conf
+sysctl -p /etc/sysctl.conf
 
 #start tailscale
-tailscale up --advertise-routes=192.168.2.204.0/24 --accept-routes
+sleep 5
+tailscale up --advertise-routes=192.168.204.0/24 --accept-routes 2> tailscale_output >&2
+echo "#############################################"
+echo ""
+cat tailscale_output
+echo ""
+echo "#############################################"
