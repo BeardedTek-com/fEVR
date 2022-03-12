43bb07ddf7c58f5a8d1997a3791dcf5eb0645638
BeardedTek <kenny@beardedtek.com>
2022-03-11 15:15:52 -0900

    change tailscale startup script

--- docker/tailscale/rootfs/data/startup.sh
+++ docker/tailscale/rootfs/data/startup.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
-# setup ip forwarding
-echo 'net.ipv4.ip_forward = 1' | tee -a /etc/sysctl.conf
-echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.conf
+
+# enable ip forwarding
+echo 'net.ipv4.ip_forward = 1' | tee -a /etc/sysctl.conf 2> /dev/null
+echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.conf 2> /dev/null
 sysctl -p /etc/sysctl.conf
 
-#start tailscale
-sleep 5
-tailscale up --advertise-routes=192.168.204.0/24 --accept-routes 2> tailscale_output >&2
+# start tailscale
+tailscale up --advertise-routes=192.168.204.0/24 --accept-routes 2> /tmp/auth &
+sleep 3
+authURL="$(cat /tmp/auth | grep http | sed 's/^[ \t]*//')"
 echo "#############################################"
 echo ""
-cat tailscale_output
+echo "Tailscale Authorization URL: $authURL"
 echo ""
 echo "#############################################"
+n=0
+while true
+do
+    sleep 300
+done
--- vol/tailscale/data/startup.sh
+++ vol/tailscale/data/startup.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
-#start tailscale
-tailscale up --advertise-routes=192.168.204.0/24 --accept-routes 2> auth_url >&2
-sleep 5
+
+# enable ip forwarding
+echo 'net.ipv4.ip_forward = 1' | tee -a /etc/sysctl.conf 2> /dev/null
+echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.conf 2> /dev/null
+sysctl -p /etc/sysctl.conf
+
+# start tailscale
+tailscale up --advertise-routes=192.168.204.0/24 --accept-routes 2> /tmp/auth &
+sleep 3
+authURL="$(cat /tmp/auth | grep http | sed 's/^[ \t]*//')"
 echo "#############################################"
 echo ""
-cat tailscale_output
+echo "Tailscale Authorization URL: $authURL"
 echo ""
 echo "#############################################"
+n=0
+while true
+do
+    sleep 300
+done
