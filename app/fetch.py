8eb7f88ef8f3fcf3b5a6a4bdc6736be78299a132
BeardedTek <kenny@beardedtek.com>
2022-06-12 13:14:06 -0800

    Catch fetch error

--- app/fetch.py
+++ app/fetch.py
@@ -15,15 +15,25 @@ class Fetch:
         self.clip = f"{self.frigate}api/events/{eventid}/clip.mp4"
         self.getEvent()
     def getEvent(self):
+        failure = "none"
         if not os.path.exists(self.thumbPATH):
             if not os.path.exists(self.path):
                 os.makedirs(self.path)
             open(self.snapPATH,'wb').write(requests.get(self.snap, allow_redirects=True).content)
-            self.resizeImg(self.snapPATH,self.thumbSize)
+            resize = self.resizeImg(self.snapPATH,self.thumbSize)
+            if resize != "OK":
+                retval="resize image"                
             open(self.clipPATH,'wb').write(requests.get(self.clip, allow_redirects=True).content)
-            return f"Got {self.event} from frigate at {self.frigate}"
+            if failure != "none":
+                return f"Failed to retrieve {self.event} from frigate at {self.frigate}: {failure}"
+            else:
+                return f"Got {self.event} from frigate at {self.frigate}"
     def resizeImg(self,img,height=180,ratio=1.777777778):
         if os.path.exists(img):
             # Resizes an image from the filesystem
-            Image.open(img).resize((int(height*ratio),height), Image.ANTIALIAS).save(self.thumbPATH,"JPEG", quality=75,optimize=True)
+            if os.path.exists(img):
+                Image.open(img).resize((int(height*ratio),height), Image.ANTIALIAS).save(self.thumbPATH,"JPEG", quality=75,optimize=True)
+                return "OK"
+            else:
+                return "fetch.py | resizeImg(): Image Path Does Not Exist"
         
